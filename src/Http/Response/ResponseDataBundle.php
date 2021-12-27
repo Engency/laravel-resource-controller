262e3e83a5ba6478e452835dd287c4ddab413437
Frank Kuipers <frank@engency.com>
2021-12-27 14:10:14 +0100

    Fixed issue in response

--- src/Http/Response/ResponseDataBundle.php
+++ src/Http/Response/ResponseDataBundle.php
@@ -99,7 +99,7 @@ class ResponseDataBundle implements Arrayable
                 return $subItem->toArrayFormat($this->customDataFormat);
             }
 
-            if (property_exists($subItem, 'toArray')) {
+            if (is_object($subItem) && property_exists($subItem, 'toArray')) {
                 return $subItem->toArray();
             }
 
